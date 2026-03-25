9d84b1affb1ee3d8008fa45a9d87ed20725f7197
Thomas Richter <thomasesr@gmail.com>
2026-03-25 04:05:09 -0300

    make luci-app-kea visible in LuCI, add a real Overview page, add the rpcd ACL

--- luci-app-kea/root/www/luci-static/resources/view/kea/overview.js
+++ luci-app-kea/root/www/luci-static/resources/view/kea/overview.js
@@ -1,8 +1,48 @@
 'use strict';
 'require view';
+'require rpc';
+'require ui';
+
+const callKeaStatus = rpc.declare({
+	object: 'luci.kea',
+	method: 'status',
+	expect: { '': {} }
+});
 
 return view.extend({
-render: function() {
-return E('div', {}, 'Kea overview placeholder');
-}
+	load: function() {
+		return Promise.all([
+			L.resolveDefault(callKeaStatus(), {})
+		]);
+	},
+
+	render: function(data) {
+		const st = data[0] || {};
+
+		return E('div', { 'class': 'cbi-map' }, [
+			E('h2', _('Kea DHCP')),
+			E('div', { 'class': 'cbi-section' }, [
+				E('div', { 'class': 'cbi-section-node' }, [
+					E('div', { 'class': 'table' }, [
+						E('div', { 'class': 'tr' }, [
+							E('div', { 'class': 'td left', 'style': 'width:30%' }, _('Service running')),
+							E('div', { 'class': 'td left' }, st.running ? _('Yes') : _('No'))
+						]),
+						E('div', { 'class': 'tr' }, [
+							E('div', { 'class': 'td left' }, _('Control Agent reachable')),
+							E('div', { 'class': 'td left' }, st.ctrl_reachable ? _('Yes') : _('No'))
+						]),
+						E('div', { 'class': 'tr' }, [
+							E('div', { 'class': 'td left' }, _('Configured subnets')),
+							E('div', { 'class': 'td left' }, '%s'.format(st.subnets || 0))
+						]),
+						E('div', { 'class': 'tr' }, [
+							E('div', { 'class': 'td left' }, _('Configured reservations')),
+							E('div', { 'class': 'td left' }, '%s'.format(st.reservations || 0))
+						])
+					])
+				])
+			])
+		]);
+	}
 });
